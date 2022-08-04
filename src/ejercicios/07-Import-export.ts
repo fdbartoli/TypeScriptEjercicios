a507a1cbab59670f6e5c9ad5b16983646250d3cf
NEORIS\fernando.bartoli <fdbartoli91@gmail.com>
2022-08-04 15:04:57 -0300

    ejercicio7

---
+++ src/ejercicios/07-Import-export.ts
@@ -0,0 +1,22 @@
+import { Producto, calculaISV } from "./06-desestructuracion-funcion";
+
+
+/*
+    ==== Código de TypeScript ====
+*/
+
+
+const carritoCompras: Producto [] = [
+    {
+    desc: 'telefono 1',
+    precio: 100
+    },
+    {
+        desc: 'telefono 2',
+        precio: 150
+}];
+
+const [total, isv] = calculaISV (carritoCompras);
+
+console.log('Total', total);
+console.log ('ISV', isv);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { Producto, calculaISV } from "./ejercicios/06-desestructuracion-funcion";
+
+
 /*
     ==== Código de TypeScript ====
 */
